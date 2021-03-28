83ff610471dce750c0cb901951c9518e71589344
Tomáš Batěk <batekto2@fit.cvut.cz>
2021-03-29 00:02:02 +0200

    Basic encrypting structure

--- task3_block/src/AESEncryptor.cpp
+++ task3_block/src/AESEncryptor.cpp
@@ -2,9 +2,7 @@
 // Created by Tomáš Batěk on 28.03.2021.
 //
 
-#include <fstream>
-#include <iostream>
-#include <cstdio>
+#include <openssl/evp.h>
 
 #include "AESEncryptor.hpp"
 #include "TGAReader.hpp"
@@ -22,5 +20,42 @@ bool AESEncryptor::load_tga_file(const std::string& filename) {
 }
 
 bool AESEncryptor::encrypt(const std::string& op_mode) {
+
+    unsigned char* key;
+    unsigned char* iv;
+
+    unsigned char* cipher_text;
+    unsigned char* plain_text;
+
+    EVP_CIPHER_CTX* ctx;
+    int len, cipher_text_len, plain_text_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        return false;
+
+    /* Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * In this example we are using 128 bit AES (i.e. a 128 bit key).
+     */
+    if(EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), nullptr, key, iv) != 1)
+        return false;
+
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, cipher_text, &len, plain_text, plain_text_len))
+        return false;
+    cipher_text_len = len;
+
+    /* Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, cipher_text + len, &len))
+        return false;
+    cipher_text_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
     return false;
 }
